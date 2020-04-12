03408238f1255d9220575b5965202aa855b22627
thyagoluciano <thyagoluciano@gmail.com>
2020-04-12 09:39:04 -0300

    fix tests

--- test/sm2_test.dart
+++ test/sm2_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sm2/SmResponse.dart';
-import 'package:sm2/sm2.dart';
+import 'package:sm2/sm.dart';
 
 void main() {
   test('Calc success', () {
